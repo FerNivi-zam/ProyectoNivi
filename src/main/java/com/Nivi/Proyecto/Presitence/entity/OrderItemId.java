403cfd171556b15bc2a2ffc3385532b7d3ac6dd4
SASF\amoreira <anthony.moreira@sasf.net>
2025-06-16 20:32:21 -0500

    De la empresa a mi máquina

--- src/main/java/com/Nivi/Proyecto/Presitence/entity/OrderItemId.java
+++ src/main/java/com/Nivi/Proyecto/Presitence/entity/OrderItemId.java
@@ -16,10 +16,12 @@ public class OrderItemId implements Serializable {
     private Integer idOrder;
     private Integer idItem;
 
+    // Se cambió lo que tenias a una versiona nterior de java porque me salia error en al compilacion, revisa si te sale igual
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof OrderItemId that)) return false;
+        if (!(o instanceof OrderItemId)) return false;
+        OrderItemId that = (OrderItemId) o;
         return Objects.equals(idOrder, that.idOrder) && Objects.equals(idItem, that.idItem);
     }
 
